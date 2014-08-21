b0b8bfc1da5e95567a7f5bdd258a0d090c031516
XTao <xutao881001@gmail.com>
2014-08-21 21:58:23 +0800

    Add markdown.

--- mikoto/mikoto.py
+++ mikoto/mikoto.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from .markdown import render_markdown
+
 
 class Mikoto(object):
 
+    def __init__(self, text):
+        self.text = text
 
-
+    @property
+    def markdown(self):
+        return render_markdown(self.text)
